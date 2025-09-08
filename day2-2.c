ab7958c3df2910b33011dafde6a9260b4a582359
Priyansh-Dixit-coder <Priyansh.21860@stu.upes.ac.in>
2025-09-08 23:47:27 +0530

    my 100 days of code

---
+++ day2-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int r;
+     printf("ENTER THE RADIUS : ");
+    printf("ENTER THE RADIUS :");
+    scanf("%d" , &r);
+    printf("THE AREA IS :%f" , 3.14*r*r );
+    printf("THE CIRCUMFERENCE IS :%f" , 2*3.14*r );
+    return 0;
+}
