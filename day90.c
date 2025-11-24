aa7dd8dd6ca15b41bfa5e1e1c07d39efec1b96a5
Priyansh-Dixit-coder <Priyansh.21860@stu.upes.ac.in>
2025-11-24 22:27:11 +0530

    day 90

---
+++ day90.c
@@ -0,0 +1,32 @@
+// Q140: Define a struct with enum Gender and print person's gender.
+#include <stdio.h>
+
+enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+};
+
+struct Person {
+    char name[50];
+    enum Gender gender;
+};
+
+int main() {
+    struct Person p;
+    p.gender = MALE;   // Example input
+
+    switch (p.gender) {
+        case MALE:
+            printf("Male\n");
+            break;
+        case FEMALE:
+            printf("Female\n");
+            break;
+        case OTHER:
+            printf("Other\n");
+            break;
+    }
+
+    return 0;
+}
